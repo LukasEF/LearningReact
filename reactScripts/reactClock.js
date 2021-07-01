ccb50803108d12b55132fbc6f14fb1e4a02d4728
fraglet <lukasefoster@gmail.com>
2021-07-01 17:08:48 +0100

    Changed clock to a stateful component

--- reactScripts/reactClock.js
+++ reactScripts/reactClock.js
@@ -1,11 +1,37 @@
-function tick() {
-    const clockElement = (
-        <div>
-            <h1>Timer</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(clockElement, document.getElementById('clock'));
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Clock</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('clock')
+);
